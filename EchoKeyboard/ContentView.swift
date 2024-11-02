d8e681897fa0c2f338bfe8a6f432758c872ad8e7
Bobby Wang <yyaustin@bu.edu>
2024-11-02 05:39:00 -0400

    Initial Commit

---
+++ EchoKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoKeyboard
+//
+//  Created by Bobby Wang on 11/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoKeyboard/EchoKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoKeyboardApp.swift
+//  EchoKeyboard
+//
+//  Created by Bobby Wang on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
