8ea5532cf227603fb995ce7e8e3ac7ef79544849
Bobby Wang <yyaustin@bu.edu>
2024-11-02 06:14:28 -0400

    配置好第一个测试键盘

--- EchoExtension/KeyboardViewController.swift
+++ EchoExtension/KeyboardViewController.swift
@@ -8,53 +8,34 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
-
-    @IBOutlet var nextKeyboardButton: UIButton!
     
-    override func updateViewConstraints() {
-        super.updateViewConstraints()
-        
-        // Add custom view sizing constraints here
-    }
+    private var testButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Perform custom UI setup here
-        self.nextKeyboardButton = UIButton(type: .system)
+        // 设置视图背景
+        view.backgroundColor = .systemBackground
         
-        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-        self.nextKeyboardButton.sizeToFit()
-        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        // 创建测试按钮
+        testButton = UIButton(type: .system)
+        testButton.setTitle("测试输入", for: .normal)
+        testButton.addTarget(self, action: #selector(handleTestInput), for: .touchUpInside)
         
-        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        // 设置按钮约束
+        testButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(testButton)
         
-        self.view.addSubview(self.nextKeyboardButton)
-        
-        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            testButton.widthAnchor.constraint(equalToConstant: 100),
+            testButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
     }
     
-    override func viewWillLayoutSubviews() {
-        self.nextKeyboardButton.isHidden = !self.needsInputModeSwitchKey
-        super.viewWillLayoutSubviews()
+    @objc private func handleTestInput() {
+        // 插入测试文本
+        textDocumentProxy.insertText("Hello from Echo!")
     }
-    
-    override func textWillChange(_ textInput: UITextInput?) {
-        // The app is about to change the document's contents. Perform any preparation here.
-    }
-    
-    override func textDidChange(_ textInput: UITextInput?) {
-        // The app has just changed the document's contents, the document context has been updated.
-        
-        var textColor: UIColor
-        let proxy = self.textDocumentProxy
-        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-            textColor = UIColor.white
-        } else {
-            textColor = UIColor.black
-        }
-        self.nextKeyboardButton.setTitleColor(textColor, for: [])
-    }
-
 }
